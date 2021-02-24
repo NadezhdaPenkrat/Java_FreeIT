30fbfc9f85ada9cfe00bf672cb237bd9f5490a52
Nadya Penkrat <nadya.penkrat@gmail.com>
2021-02-24 18:59:17 +0300

    gitignore updated

--- lesson_1/com/freeit/src/Hello.java
+++ lesson_1/com/freeit/src/Hello.java
@@ -1,6 +1,7 @@
+package com.freeit.src;
+
 public class Hello {
     public static void main (String [] args){
-        System.out.println ("Hello World");
-        
+        System.out.println ("Hello ! " );
     }
 }
