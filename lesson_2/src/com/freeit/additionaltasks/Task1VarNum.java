9717c740a9446d2a62cb7486f74ded26b4d57021
Nadya Penkrat <nadya.penkrat@gmail.com>
2021-03-02 14:55:30 +0300

    changed: checked number is positive or not then checked amount of digits and print results

--- lesson_2/src/com/freeit/additionaltasks/Task1VarNum.java
+++ lesson_2/src/com/freeit/additionaltasks/Task1VarNum.java
@@ -12,26 +12,32 @@ package com.freeit.additionaltasks;
 import java.util.Random;
 
 public class Task1VarNum {
-    public static void main (String [] args) {
+    public static void main(String[] args) {
         Random random = new Random();
-        int number = random.nextInt(1000);
-        System.out.println ( " Number " +number );
+        int number = (int)  (-1000+ random.nextInt(1000));
+        System.out.println(" Number " + number);
+        if (number >= 0) {
+            System.out.println(number + " is a positive number");
+            }else if (number > 0 && number < 10) {
+                System.out.println(" This is a monosemantic positive number. ");
+            }else if (number > 10 && number < 100) {
+                System.out.println(" This is a two-digit positive number. ");
+            }else   {
+                System.out.println(" This is a three-digit positive number. ");
+        }
+        if (number <= 0){
+            System.out.println(number + " is a negative number");
+            } else if (number > -10 && number < 0) {
+                System.out.println(" This is a monosemantic negative number. ");
+            }else if (number > -100 && number < -10) {
+                System.out.println(" This is a two-digit negative number. ");
+            }else  {
+                System.out.println(" This is a three-digit negative number. ");
 
-        if (number>-10 && number <0) {
-            System.out.println ( " This is a monosemantic negative number. " );
             }
-        if (number>-100 && number<-10) {
-            System.out.println ( " This is a two-digit negative number. " );
-        }
-        if (number>0 && number<10) {
-            System.out.println ( " This is a monosemantic positive number. " );
-        }
-        if (number>10 && number<100) {
-            System.out.println ( " This is a two-digit positive number. " );
-        }
-        if (number>100 && number<1000) {
-            System.out.println ( " This is a three-digit positive number. " );
-        }
     }
 }
+
+
+
 
