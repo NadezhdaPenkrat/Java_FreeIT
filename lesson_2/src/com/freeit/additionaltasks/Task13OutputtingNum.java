43f39793a9221db5fe955b84c59df3df0370adff
Nadya Penkrat <nadya.penkrat@gmail.com>
2021-03-02 13:41:05 +0300

    remote don't needed array

--- lesson_2/src/com/freeit/additionaltasks/Task13OutputtingNum.java
+++ lesson_2/src/com/freeit/additionaltasks/Task13OutputtingNum.java
@@ -1,12 +1,13 @@
 package com.freeit.additionaltasks;
+
+import java.util.Random;
+
 // 13)Напишите программу вывода всех четных чисел от 2 до 100 включительно
 public class Task13OutputtingNum {
     public static void main(String[] args) {
-        int[] array = new int[21];
-        for (int i = 2; i <= 20; i = i + 2){
-            i = i + array[i];
 
-            System.out.println("outputting all even numbers from 2 to 100 : "+ i + "  ");
+        for (int i = 2; i <= 101; i = i + 2){
+         System.out.println("outputting all even numbers from 2 to 100 : "+ i + "  ");
         }
 
     }
