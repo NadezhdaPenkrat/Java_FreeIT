4a8c1070e28c222986906104edb334c807f83d6c
Nadya Penkrat <nadya.penkrat@gmail.com>
2021-03-04 16:40:25 +0300

    changed

--- lesson_1/com/freeit/src/Task3TimePrinter.java
+++ lesson_1/com/freeit/src/Task3TimePrinter.java
@@ -1,4 +1,5 @@
 package com.freeit.src;
+
 //Имеется 4500 секунд. Вывести в консоль содержащихся в этом количестве секунд:
 //А) минут + секунд,
 //В) часов + минут + секунд,
@@ -11,17 +12,17 @@ public class Task3TimePrinter {
         int sec = s % 60;
         int m = (s - sec) / 60; //А) минут + секунд.
         int min = m % 60;
-        int h = (m - min) / 60;  //В) часов + минут + секунд.
-        int day = h % 24;
-        int d = (h - day) / 24; //С) дней + часов + минут + секунд.
-        int week = d % 7;
-        int w = (d - week) / 7; //D) недель + дней + часов + минут + секунд.
-
+        int h = m / 60;  //В) часов + минут + секунд.
+        int hour = h % 60;
+        int d = h / 24; //С) дней + часов + минут + секунд.
+        int day = d % 24;
+        int w = d / 7; //D) недель + дней + часов + минут + секунд.
+        int week = w % 7;
         // Вывести в консоль содержащихся в этом количестве секунд:
-        System.out.println(m + " минут " + sec + "секунд");
-        System.out.println(h + " часов " + min + " минут " + sec + "секунд");
-        System.out.println(d + " дней " + h + " часов " + min + " минут " + sec + "секунд");
-        System.out.println(w + " недель " + d + " дней " + h + " часов " + min + " минут " + sec + "секунд");
+        System.out.println(min + " минут " + sec + "секунд");
+        System.out.println(hour + " часов " + min + " минут " + sec + "секунд");
+        System.out.println(day + " дней " + hour + " часов " + min + " минут " + sec + "секунд");
+        System.out.println(week + " недель " + day + " дней " + hour + " часов " + min + " минут " + sec + "секунд");
     }
 }
 
