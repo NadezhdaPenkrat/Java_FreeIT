dc4d084da18c410b6dbb9b4702908207afc78b92
Nadya Penkrat <nadya.penkrat@gmail.com>
2021-03-04 16:40:38 +0300

    upd

--- lesson_1/com/freeit/src/Task1Hello.java
+++ lesson_1/com/freeit/src/Task1Hello.java
@@ -1,7 +1,7 @@
 package com.freeit.src;
 
 public class Task1Hello {
-    public static void main (String [] args){
-        System.out.println ("Hello ! " );
+    public static void main(String[] args) {
+        System.out.println("Hello ! ");
     }
 }
